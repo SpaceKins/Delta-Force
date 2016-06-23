609cc2885f00390037793909c37bf5fc3cd1cb30
Spacekins <spacekins@zoho.com>
2016-06-22 20:09:22 -1000

    Added Complete event and listener

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var count=0;
 thisTimer.on('tick',function(){console.log('tick-tock'); count++;});  //+ JSON.stringify(thisTimer.getIntervalId())
 thisTimer.on('start',function(startTime){console.log('Start at ' + startTime);});
 thisTimer.on('stop',function(stopTime){console.log('Stopped at ' + stopTime);});  
+thisTimer.on('complete',function(durationTime){console.log('Duration ' + durationTime);});  
 
-setTimeout(function(){thisTimer.startTimer();},5000);
+
+setTimeout(function(){thisTimer.startTimer(3);},5000);
 setTimeout(thisTimer.stopTimer,15000);
