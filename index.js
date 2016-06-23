f1ccf8e04b101c1e89689055474a23cca4338700
Spacekins <spacekins@zoho.com>
2016-06-22 20:13:09 -1000

    Added count to tick

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var BasicTimer=require('./lib/BasicTimer');
 var thisTimer= new BasicTimer();
 var count=0;
 
-thisTimer.on('tick',function(){console.log('tick-tock'); count++;});  //+ JSON.stringify(thisTimer.getIntervalId())
+thisTimer.on('tick',function(tickCount){console.log('tick-tock ' + tickCount ); });  //+ JSON.stringify(thisTimer.getIntervalId())
 thisTimer.on('start',function(startTime){console.log('Start at ' + startTime);});
 thisTimer.on('stop',function(stopTime){console.log('Stopped at ' + stopTime);});  
 thisTimer.on('complete',function(durationTime){console.log('Duration ' + durationTime);});  
 
 
-setTimeout(function(){thisTimer.startTimer(3);},5000);
+setTimeout(function(){thisTimer.startTimer();},5000);
 setTimeout(thisTimer.stopTimer,15000);
