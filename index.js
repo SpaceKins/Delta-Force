3b8eb9df06778833331e8e09aaed90cbf99673f9
Spacekins <spacekins@zoho.com>
2016-06-22 19:14:39 -1000

    Add feature to star and stop.  Removed automatic start on initialization

--- index.js
+++ index.js
@@ -3,20 +3,9 @@ var BasicTimer=require('./lib/BasicTimer');
 var thisTimer= new BasicTimer();
 var count=0;
 
-thisTimer.on('tick',function(name){console.log('tick-tock'); count++;});  //+ JSON.stringify(thisTimer.getIntervalId())
-
-/*
-  thisTimer.on('start',function(name){console.log(name + count); count++;});  //+ JSON.stringify(thisTimer.getIntervalId())
-  thisTimer.on('stop',function(name){console.log(name + count); count++;});
-  thisTimer.on('complete',function(name){console.log(name + count); count++;});
-  thisTimer.on('lag',function(thisLagTime){console.log('This Lag time =' + thisLagTime); count++;});
-
-
-function stop()
-{
- thisTimer.stopTimer();
-  console.log("stopped at " + count);
-}
+thisTimer.on('tick',function(){console.log('tick-tock'); count++;});  //+ JSON.stringify(thisTimer.getIntervalId())
+thisTimer.on('start',function(startTime){console.log('Start at ' + startTime);});
+thisTimer.on('stop',function(stopTime){console.log('Stopped at ' + stopTime);});  
 
 setTimeout(thisTimer.startTimer,5000);
-setTimeout(thisTimer.stopTimer,8000);*/
+setTimeout(thisTimer.stopTimer,8000);
